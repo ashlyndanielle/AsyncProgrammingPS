fd8095d69c72b02cc6742a916afac1d508bd1f56
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-04-11 22:25:01 -0600

    completed module "Creating and Queuing Promises" and updated notes file

--- notes.md
+++ notes.md
@@ -18,3 +18,26 @@ __Promise States__
 * Rejected - returns a reason why it was rejected
 
 __*Always remember to return a promise when chaining .then()*__
+
+__Creating Promises__
+* a promise takes a function as it's only parameter to it's constructor (executer function)
+  * the executer function takes two parameters: resolve and reject - change state of promise to fulfilled or rejected
+
+__Promise.all()__
+* pass array of calls into function
+* results are returned in an array in the same order that you passed the calls in
+* waits until all promises are fulfilled OR until one promise is rejected
+
+__Promise.allSettled()__
+* not all browsers support this (of course...)
+* returns an object with two keys
+  * a status key of 'fulfilled' or 'rejected'
+  * the second key is 'value' if fulfilled; or 'reason' if rejected
+* .then() will return all promises, even if they were rejected
+* we don't need a catch block but should still include it
+
+__Promise.race()__
+* used when trying to get data from the quickest source (i.e. which server is closest)
+* will return whenever the first promise *settles*
+  * if the first promise fails, it will call the catch function and you won't get your data
+* not a lot of use cases but just be aware of it
--- src/creating.mjs
+++ src/creating.mjs
@@ -1,22 +1,108 @@
 import setText, { appendText } from "./results.mjs";
 
 export function timeout(){
+  const wait = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Timeout!');
+    }, 1500);
+  });
+
+  wait.then(text => setText(text));
 }
 
 export function interval(){
+  let counter = 0;
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      console.log('logging interval...')
+      resolve(`Counter: ${++counter}`)
+    }, 1500)
+  })
+  wait.then(response => setText(response))
+      .finally(() => appendText(`\nDone ${counter}`));
 }
 
 export function clearIntervalChain(){
+  let counter = 0;
+  let interval;
+  const wait = new Promise((resolve) => {
+    interval = setInterval(() => {
+      console.log(`logging counter: ${counter}`);
+      resolve(`Counter: ${++counter}`);
+    }, 1500)
+  })
+
+  wait.then(response => setText(response))
+      .finally(() => clearInterval(interval));
 }
 
 export function xhr(){
+  let request = new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/users/7');
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        resolve(xhr.responseText)
+      } else {
+        reject(xhr.status)
+      }
+    };
+    // onerror is only called when there is something like a network
+    // error and the call fails to be completed
+    xhr.onerror = () => reject('Request Failed');
+
+    xhr.send();
+  })
+
+  request.then(result => setText(result))
+         .catch(error => setText(error));
 }
 
 export function allPromises(){
+  let categories = axios.get('http://localhost:3000/itemCategories');
+  let statuses = axios.get('http://localhost:3000/orderStatuses');
+  let userTypes = axios.get('http://localhost:3000/userTypes');
+  let addressTypes = axios.get('http://localhost:3000/addressTypes')
+
+  Promise.all([categories, statuses, userTypes, addressTypes])
+    .then(([catResults, statResults, userResults, addressResults]) => {
+      setText(`All Promises Resolved:`);
+
+      appendText(JSON.stringify(catResults.data));
+      appendText(JSON.stringify(statResults.data));
+      appendText(JSON.stringify(userResults.data));
+      appendText(JSON.stringify(addressResults.data));
+    })
+    .catch(error => setText(error))
 }
 
 export function allSettled(){
+  let categories = axios.get('http://localhost:3000/itemCategories');
+  let statuses = axios.get('http://localhost:3000/orderStatuses');
+  let userTypes = axios.get('http://localhost:3000/userTypes');
+  let addressTypes = axios.get('http://localhost:3000/addressTypes');
+
+  Promise.allSettled([categories, statuses, userTypes, addressTypes])
+    .then((values) => {
+      let results = values.map(response => {
+        if (response.status === 'fulfilled') {
+          console.log(response.value);
+          return ` FULFILLED: ${response.value.data[0].description}`;
+        }
+        return ` REJECTED: ${response.reason.message}`;
+      })
+      setText(results);
+    })
+    .catch(error => {
+      setText(error)
+    })
 }
 
 export function race(){
+  let users = axios.get('http://localhost:3000/users');
+  let backup = axios.get('http://localhost:3001/users');
+
+  Promise.race([users, backup])
+    .then(users => setText(JSON.stringify(users.data)))
+    .catch(error => setText(error))
 }
